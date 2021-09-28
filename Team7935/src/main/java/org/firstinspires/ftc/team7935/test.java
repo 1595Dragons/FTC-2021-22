b4844a53b02b562c9d61ece172d2f7f2b07fdce3
Spud <jeffrypig23@gmail.com>
2021-09-28 16:19:14 -0700

    Created test.java opmode

---
+++ Team7935/src/main/java/org/firstinspires/ftc/team7935/test.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.team7935;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//@Autonomous(name="", group="")
+//@TeleOp(name="", group="")
+//@Disabled
+public class test extends OpMode {
+
+	@Override
+	public void init() {
+		// Called once the INIT button is pressed.
+	}
+
+	@Override
+	public void init_loop() {
+		// Called repeatedly when the INIT button is pressed.
+	}
+
+	@Override
+	public void start() {
+		// Called once when the play button is pressed.
+	}
+
+	@Override
+	public void loop() {
+		// Called repeatedly in a loop while this op mode is running.
+	}
+
+	@Override
+	public void stop() {
+		// Called when this op mode is first disabled.
+	}
+}
